8dd95bdaf6a29cbb90777c5565505d852e8af613
ItsJustMeChris <hello@itschris.dev>
2021-01-18 15:51:56 +0400

    fix cors

--- index.ts
+++ index.ts
@@ -1,7 +1,6 @@
 import { Application } from 'https://deno.land/x/abc/mod.ts';
 import { cors, CORSConfig, DefaultCORSConfig } from 'https://deno.land/x/abc/middleware/cors.ts'
-
-import { CONFIG } from './cors.ts';
+import { HttpMethod } from "https://deno.land/x/abc/constants.ts";
 
 import 'https://deno.land/x/dotenv/load.ts';
 // https://github.com/vlucas/phpdotenv/issues/76#issuecomment-87252126 | Use a service...
@@ -15,14 +14,26 @@ routes(app);
 const productionMode: boolean = Boolean(Deno.env.get('production'));
 const port: number = Number(Deno.env.get('SERVER_PORT')) || 8080;
 
+const corsURL: string = Deno.env.get('CORS_URL') || '*';
+
+const CONFIG: CORSConfig = {
+    allowOrigins: [corsURL],
+    allowMethods: [
+        HttpMethod.Delete,
+        HttpMethod.Get,
+        HttpMethod.Head,
+        HttpMethod.Patch,
+        HttpMethod.Post,
+        HttpMethod.Put
+    ]
+};
 
 if (productionMode) {
     // ENV
     const certFile: string = Deno.env.get('SSL_CERT') || '';
     const keyFile: string = Deno.env.get('SSL_KEY') || '';
     const hostname: string = Deno.env.get('SERVER_HOSTNAME') || '';
-    const corsConfig = CONFIG !== undefined ? CONFIG : DefaultCORSConfig;
-    app.use(cors(corsConfig));
+    app.use(cors(CONFIG));
 
     app.startTLS({ port, certFile, keyFile, hostname })
 } else {
